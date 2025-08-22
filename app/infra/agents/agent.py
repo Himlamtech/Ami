a8efa806c3b27dcd391942284fa6bb8b534a5d5d
HienHoang3011 <hoangchihien301105@gmail.com>
2025-08-22 22:58:50 +0700

    add root agent

--- app/infra/agents/agent.py
+++ app/infra/agents/agent.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 root_agent = Agent(
-    name = "root_agent",     # Use settings.THINKING_CHAT_MODEL_ID later because we cannot test with adk web
+    name = "root_agent",   
+    model = "gemini-2.5-flash",# Use settings.THINKING_CHAT_MODEL_ID later because we cannot test with adk web
     description= "Tác nhân gốc, có chức năng phân tích và định tuyến yêu cầu đến tác nhân phù hợp.",
     instruction= root_agent_instruction,
     planner= BuiltInPlanner(
